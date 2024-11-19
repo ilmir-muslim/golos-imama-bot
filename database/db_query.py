a43aeaff0e35624687ac1ff9839fb5aceea7c65f
ilmir-muslim <ilmir_gf@mail.ru>
2024-11-19 11:27:29 +0200

    добавлен вывод файлов урока, добавление file_id в базу данных

--- database/db_query.py
+++ database/db_query.py
@@ -1,37 +1,68 @@
-import asyncpg
-import asyncio
+from sqlalchemy import text
+from sqlalchemy.exc import SQLAlchemyError
 
 
-async def fetch_chapter():
-    names = []
-    conn = await asyncpg.connect(
-        database="golos_imama",
-        user="ilmir",
-        password="230103",
-        host="localhost",
-        port=5432,
-    )
-
-    async with conn.transaction():
-        rows = await conn.fetch("SELECT name FROM chapter")
-        names = [row["name"] for row in rows]
-
-    await conn.close()
+async def fetch_chapter(session):
+    query = text("SELECT name FROM chapter")
+    async with session.begin():
+        result = await session.execute(query)
+    names = [row[0] for row in result]
     return names
 
-async def fetch_theme(chapter):
-    names = []
-    conn = await asyncpg.connect(
-        database="golos_imama",
-        user="ilmir",
-        password="230103",
-        host="localhost",
-        port=5432,
+
+async def fetch_theme(session, chapter):
+    query = text(
+        """
+        SELECT name 
+        FROM theme 
+        WHERE chapter_id = (SELECT id FROM chapter WHERE name = :chapter)
+    """
     )
-
-    async with conn.transaction():
-        rows = await conn.fetch(f"SELECT name FROM theme WHERE chapter_id = (SELECT id FROM chapter WHERE name = '{chapter}')")
-        names = [row["name"] for row in rows]
-
-    await conn.close()
+    async with session.begin():
+        result = await session.execute(query, {"chapter": chapter})
+    names = [row[0] for row in result]
     return names
+
+
+async def get_lesson_data(session, theme):
+    # Выполняем запрос для получения данных из базы данных
+    query = text(
+        """
+    SELECT lesson.name, lesson.annotation, lesson.audio_lesson, lesson.text_lesson, 
+        telegram_file_id.audiofile_id, telegram_file_id.text_file_id
+    FROM lesson  
+    LEFT JOIN telegram_file_id ON telegram_file_id.name_id = lesson.id 
+    WHERE lesson.theme_id = (SELECT id FROM theme WHERE name = :theme)
+    """
+    )
+    async with session.begin():
+        result = await session.execute(query, {"theme": theme})
+    lessons = []
+    for row in result:
+        lesson = {
+            "name": row[0],
+            "annotation": row[1],
+            "audio_lesson": row[2],
+            "text_lesson": row[3],
+            "audiofile_id": row[4],
+            "text_file_id": row[5],
+        }
+        lessons.append(lesson)
+
+    return lessons
+
+
+async def add_file_id(session, name, audiofile_id, text_file_id):
+    query = text(
+        """ INSERT INTO telegram_file_id (name_id, audiofile_id, text_file_id) VALUES ((SELECT id FROM lesson WHERE name = :name) , :audiofile_id, :text_file_id) """
+    )
+    try:
+        async with session.begin():
+            await session.execute(
+                query,
+                {"name": name, "audiofile_id": audiofile_id, "text_file_id": text_file_id},
+            )
+            print(f"Inserting data: {name}, {audiofile_id}, {text_file_id}")
+
+    except SQLAlchemyError as e:
+        print(f"Ошибка при выполнении SQL-запроса: {e}")
--- handlers/handler.py
+++ handlers/handler.py
@@ -1,24 +1,77 @@
-from aiogram import Router, types
+from aiogram import Router, types, F
 from aiogram.filters import CommandStart
-from aiogram import F
+from aiogram.types import FSInputFile
 
-from database.db_query import fetch_chapter, fetch_theme  # Функция для получения данных из БД
+
+from database.db_query import (
+    add_file_id,
+    fetch_chapter,
+    fetch_theme,
+    get_lesson_data,
+)  # Функция для получения данных из БД
+from database.engine import session_maker
 from kbds.reply import get_keyboard  # Импорт функции клавиатуры
 
-# Создаем роутер для обработки команд
-start_router = Router()
 
-@start_router.message(CommandStart())
-async def send_chapter_btns(message: types.Message):
-    names = await fetch_chapter()
-    keyboard = get_keyboard(*names,sizes=(3, ),) # Создаем клавиатуру на основе данных
-    await message.answer("Выберите раздел:", reply_markup=keyboard)
-    
+class MyRouter(Router):
+    @staticmethod
+    async def start_handler(message: types.Message):
+        async with session_maker() as session:
+            chapter_names = await fetch_chapter(session)
+            keyboard = get_keyboard(
+                *chapter_names,
+                sizes=(3,),
+            )  # Создаем клавиатуру на основе данных
+            await message.answer("Выберите раздел:", reply_markup=keyboard)
 
-@start_router.message(F.text)
-async def send_theme_btns(message: types.Message):
-    chapter = message.text
-    print(f'выбран раздел {chapter}')
-    names = await fetch_theme(chapter=chapter)
-    keyboard = get_keyboard(*names,sizes=(3, ),) # Создаем клавиатуру на основе данных
-    await message.answer("Выберите тему:", reply_markup=keyboard)
+    @staticmethod
+    async def theme_handler(message: types.Message):
+        async with session_maker() as session:
+            chapter = message.text
+            theme_names = await fetch_theme(session, chapter)
+            keyboard = get_keyboard(
+                *theme_names,
+                sizes=(3,),
+            )  # Создаем клавиатуру на основе данных
+            await message.answer("Выберите тему:", reply_markup=keyboard)
+
+    @staticmethod
+    async def lesson_handler(message: types.Message):
+        async with session_maker() as session:
+            theme = message.text
+
+            lessons = await get_lesson_data(session, theme)
+            if lessons:
+                for lesson in lessons:
+                    await message.answer(lesson["name"])
+                    await message.answer(lesson["annotation"])
+                    if lesson["audiofile_id"] and lesson["text_file_id"]:
+                        await message.answer_audio(audio=lesson["audiofile_id"])
+                        await message.answer_document(document=lesson["text_file_id"])
+                    else:
+                        audio_path = f'/home/ilmir/dev_vscode/golos_imama/admin_panel/{lesson["audio_lesson"]}'
+                        audio_file = FSInputFile(audio_path)
+                        msg_a = await message.answer_audio(
+                            audio_file, caption=lesson["name"]
+                        )
+                        audiofile_id = msg_a.audio.file_id
+
+                        text_path = f'/home/ilmir/dev_vscode/golos_imama/admin_panel/{lesson["text_lesson"]}'
+                        text_file = FSInputFile(text_path)
+                        msg_t = await message.answer_document(
+                            text_file, caption=lesson["name"]
+                        )
+                        text_file_id = msg_t.document.file_id
+
+                        name = lesson["name"]
+                        await add_file_id(session, name, audiofile_id, text_file_id)
+
+            else:
+                await message.answer("Нет данных по этой теме.")
+
+
+start_router = MyRouter()
+
+start_router.message.register(MyRouter.start_handler, CommandStart())
+start_router.message.register(MyRouter.theme_handler, F.text == "тестовый раздел")
+start_router.message.register(MyRouter.lesson_handler, F.text == "тестовая тема")
